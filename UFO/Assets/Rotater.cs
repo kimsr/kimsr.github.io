c12822cbe6ba93d27a26e6b44f2e43c71ff83e72
KimSR <equinox1026@naver.com>
2016-05-25 13:21:23 +0900

    UFO

---
+++ UFO/Assets/Rotater.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour
+{
+
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+
+    }
+}
